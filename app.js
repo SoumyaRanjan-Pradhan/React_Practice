520f7169d033ee86faef746500747fac5bd106d5
soumya <soumya@example.com>
2024-11-28 00:10:29 +0530

    worked on rendering of multiple elements in react

--- app.js
+++ app.js
@@ -1,28 +1,36 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 
-const container = (
-  <div
-    className="container"
-    id="container"
-    style={{
-      border: "1px solid gray",
-      borderRadius: "4px",
-      width: "300px",
-      padding: "10px",
-      backgroundColor: "yellow",
-    }}
-  >
-    <h2 style={{ margin: "5px 0" }}>React Js</h2>
-    <img src="cat.jpg" alt="cat" width="300" />
-    <p>
-      Welcome to the React documentation! This page will give you an
-      introduction to the 80% of React concepts that you will use on a daily
-      basis.
-    </p>
-  </div>
-);
-
-console.log(container);
+const card = (key, pic, title, des, price) => {
+  return (
+    <div className="card" key={key}>
+      <div className="image">
+        <img src={pic} alt="iphone" />
+      </div>
+      <div className="card_text">
+        <h3>{title}</h3>
+        <h4>${price}</h4>
+        <p>{des}</p>
+      </div>
+    </div>
+  );
+};
 const root = createRoot(document.querySelector("#root"));
-root.render(container);
+fetch("https://dummyjson.com/products/search?q=phone")
+  .then((res) => res.json())
+  .then((result) => {
+    let data = result.products;
+    console.log(data);
+    let cards = data.map((value) => {
+      return card(
+        value.id,
+        value.thumbnail,
+        value.title,
+        value.description,
+        value.price
+      );
+    });
+    const  root_container = <div><h1>Get to know iPhone.</h1>
+    <div className="container">{cards}</div></div>
+    root.render(root_container);
+  });
